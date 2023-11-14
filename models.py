c79cfc2f67f3f3702385e146aac792cadb4fb3bb
loken9387 <84040228+loken9387@users.noreply.github.com>
2023-11-14 08:51:17 -0500

    Revert "Add classes for node, group, and tempdata"
    This reverts commit 732dd09ad80df4dad75f466a5c0b00fa4a560aa5.

--- models.py
+++ models.py
@@ -1,40 +1,7 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Boolean, Float
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import validates
-from sqlalchemy.orm import relationship
-import datetime
-
 
 from app import db
-
-class Group(db.Model):
-    __tablename__ = 'groups'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    nodes = relationship('Nodes', backref='group')
-    temp_data = relationship('TempData', backref='group')
-
-    def __str__(self):
-        return self.name
-    
-class Node(db.Model):
-    __tablename__ = 'nodes'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    group_id = Column(Integer, ForeignKey('group.id'))
-    location = Column(String(255), nullable=False)
-    active = Column(Boolean, default=True)
-    sleeptime = Column(Integer, default=0)  
-    interval_time = Column(Integer, default=0)
-    temp_data = relationship('TempData', backref='node')
-
-
-class TempData(db.Model):
-    __tablename__ = 'temp_data'
-    id = Column(Integer, primary_key=True)
-    temp = Column(Float, nullable=False)
-    time = Column(DateTime, default=datetime.datetime.utcnow)
-    node_id = Column(Integer, ForeignKey('nodes.id'))
-    group_id = Column(Integer, ForeignKey('groups.id'))
 
 
 class Restaurant(db.Model):
